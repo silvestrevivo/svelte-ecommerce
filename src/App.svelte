5af0f995fa65ac53b2d0aa0be6aa6297e2549a79
silvestrevivo <silvestrevivomillan@gmail.com>
2020-04-13 16:48:59 +0200

    routing installed

--- src/App.svelte
+++ src/App.svelte
@@ -6,6 +6,22 @@
     import About from './pages/About.svelte';
     import Products from './pages/Products.svelte';
     import ProductTemplate from './pages/ProductTemplate.svelte';
+
+    // Router
+    import { Router, Route, Link } from 'svelte-routing';
 </script>
 
-<h1>Hello</h1>
+<Router>
+    <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">about</Link>
+        <Link to="/products">Products</Link>
+    </nav>
+    <div>
+        <Route path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/login" component={Login} />
+        <Route path="/products" component={Products} />
+        <Route path="/products/:id" component={ProductTemplate} />
+    </div>
+</Router>
