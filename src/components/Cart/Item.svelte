834c4c334f7e69ab4ee94a55a268913330a18b52
silvestrevivo <silvestrevivomillan@gmail.com>
2020-04-19 22:30:49 +0200

    increase and decreate functions added

--- src/components/Cart/Item.svelte
+++ src/components/Cart/Item.svelte
@@ -1,5 +1,10 @@
 <script>
     export let id, image, title, price, amount;
+    import {
+        removeItem,
+        increaseAmount,
+        decreaseAmount,
+    } from '../../stores/cart';
 </script>
 
 <!-- cart item -->
@@ -8,22 +13,16 @@
     <div>
         <h4>{title}</h4>
         <h5>${price}</h5>
-        <button
-            class="cart-btn remove-btn"
-            on:click={() => console.log('remove', id)}>
+        <button class="cart-btn remove-btn" on:click={() => removeItem(id)}>
             remove
         </button>
     </div>
     <div>
-        <button
-            class="cart-btn amount-btn"
-            on:click={() => console.log('increase item', id)}>
+        <button class="cart-btn amount-btn" on:click={() => increaseAmount(id)}>
             <i class="fas fa-chevron-up" />
         </button>
         <p class="item-amount">{amount}</p>
-        <button
-            class="cart-btn amount-btn"
-            on:click={() => console.log('decrease item', id)}>
+        <button class="cart-btn amount-btn" on:click={() => decreaseAmount(id)}>
             <i class="fas fa-chevron-down" />
         </button>
     </div>
--- src/stores/cart.js
+++ src/stores/cart.js
@@ -8,4 +8,47 @@ export const cartTotal = derived(cart, $cart => {
     return total.toFixed(2);
 });
 
+const remove = (id, items) => {
+    return items.filter(item => item.id !== id);
+};
+
+const toggleAmount = (id, items, action) => {
+    return items.map(item => {
+        let newAmount;
+        if (action === 'inc') {
+            newAmount = item.amount + 1;
+        } else if (action === 'dec') {
+            newAmount = item.amount - 1;
+        } else {
+            newAmount = item.amount;
+        }
+        return item.id === id ? { ...item, amount: newAmount } : { ...item };
+    });
+};
+
+export const removeItem = id => {
+    cart.update(storeValue => {
+        return remove(id, storeValue);
+    });
+};
+
+export const increaseAmount = id => {
+    cart.update(storeValue => {
+        return toggleAmount(id, storeValue, 'inc');
+    });
+};
+
+export const decreaseAmount = id => {
+    cart.update(storeValue => {
+        let item = storeValue.find(item => item.id === id);
+        let cart;
+        if (item.amount === 1) {
+            cart = remove(id, storeValue);
+        } else {
+            cart = toggleAmount(id, storeValue, 'dec');
+        }
+        return [...cart];
+    });
+};
+
 export default cart;
